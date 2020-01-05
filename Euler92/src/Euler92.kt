d8f3beea161245ed6a18706a3391530605720ba6
Lindsey <lindsey.xixi.wang@gmail.com>
2020-01-04 19:53:22 -0500

    Euler92

--- Euler92/src/Euler92.kt
+++ Euler92/src/Euler92.kt
@@ -1,5 +1,19 @@
 fun main() {
-    println(addSquareDigits(44))
+//    println(addSquareDigits(44))
+    println(test(10000000, 1, 1, 0))
+}
+
+fun test(max: Int, startingNum: Int, currNum: Int, count: Int): Int {
+    val newCount = count
+    if (startingNum == max)
+        return count
+    if (currNum == 10) {
+        newCount + 1
+        println(startingNum)
+    } else {
+        return test(max, startingNum + 1, addSquareDigits(startingNum + 1), newCount)
+    }
+    return 0
 }
 
 fun addSquareDigits(num: Int): Int {
