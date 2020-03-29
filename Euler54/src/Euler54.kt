984b4da8e676f903a68302493de2303fc0d250a1
Lindsey <lindsey.xixi.wang@gmail.com>
2020-03-29 18:39:38 -0400

    Determines Hand

--- Euler54/src/Euler54.kt
+++ Euler54/src/Euler54.kt
@@ -16,7 +16,7 @@ val HANDS = mutableListOf(
     "Royal Flush" // Ten, Jack, Queen, King, Ace, in same suit.
 )
 
-private class Card (val face: Char, val suit: Char): Comparable<Card> {
+class Card (val face: Char, val suit: Char): Comparable<Card> {
     override fun compareTo(other: Card): Int {
         val faceIndex = FACES.indexOf(face)
         val otherIndex = FACES.indexOf(other.face)
@@ -29,6 +29,154 @@ private class Card (val face: Char, val suit: Char): Comparable<Card> {
     }
 }
 
+class Hand(cards: List<Card>) {
+    private val sortedCards = cards.sorted()
+
+    private val faces = mutableListOf<Char>()
+    private val suits = mutableListOf<Char>()
+
+    private fun groupFacesAndSuits() {
+        for (card in sortedCards) {
+            faces.add(card.face)
+            suits.add(card.suit)
+        }
+    }
+
+    fun getHighCard(): Char { // if the same...compare second highest
+        return sortedCards.last().face
+    }
+
+    fun getOnePair(): Char { // if the same...highest of remaining three
+        var pairValue = ' '
+        groupFacesAndSuits()
+        for (face in faces) {
+            if (faces.count { it == face } != 2) {
+                continue
+            } else {
+                pairValue = face
+            }
+        }
+        return pairValue // make sure is not ' '
+    }
+
+    fun getTwoPairs(): List<Char> { // highest pair determines winner / if same two pairs...compare 5th card
+        var pairsValue = mutableListOf<Char>()
+        groupFacesAndSuits()
+        for (face in faces) {
+            if (faces.count { it == face } != 2) {
+                continue
+            } else {
+                pairsValue.add(face)
+            }
+        }
+        return pairsValue.distinct().sortedDescending() // make sure size is two
+    }
+
+    fun getThreeOfAKind(): Char { // if the same...compare last two
+        var tripletValue = ' '
+        groupFacesAndSuits()
+        for (face in faces) {
+            if (faces.count { it == face } != 3) {
+                continue
+            } else {
+                tripletValue = face
+            }
+        }
+        return tripletValue // make sure is not ' '
+    }
+
+    fun getStraight(): List<Char> { // no kickers
+        groupFacesAndSuits()
+        if (FACES.indexOf(faces.last()) - FACES.indexOf(faces.first()) == 4 && faces.distinct().size == 5) {
+            return faces
+        } else if (faces.last() == 'A' && faces.distinct().size == 5) {
+            if (faces.first() == '2' && faces[faces.size - 2] == '5') {
+                return faces
+            }
+        }
+        return listOf()
+    }
+
+    fun getFlush(): Char { // if both have flush...compare highest cards
+        groupFacesAndSuits()
+        if (suits.distinct().size == 1) {
+            return suits.distinct()[0]
+        }
+        return ' '
+    }
+
+    fun getFullHouse(): List<Char> { // if both have...compare three of a kind then pair
+        val threeOfAKind = Hand(sortedCards).getThreeOfAKind()
+        val pair = Hand(sortedCards).getOnePair()
+        return listOf(threeOfAKind, pair)
+    }
+
+    fun getFourOfAKind(): Char { // if the same...compare last card
+        var quadValue = ' '
+        groupFacesAndSuits()
+        for (face in faces) {
+            if (faces.count { it == face } != 4) {
+                continue
+            } else {
+                quadValue = face
+            }
+        }
+        return quadValue // make sure is not ' '
+    }
+
+    fun getStraightFlush(): List<Char> { // same as straight
+        groupFacesAndSuits()
+        if (suits.distinct().size == 1) {
+            val straight = Hand(sortedCards).getStraight()
+            if (straight.size == 5) {
+                return straight
+            }
+        }
+        return listOf()
+    }
+
+    fun isRoyalFlush(): Boolean {
+        val straightFlush = Hand(sortedCards).getStraightFlush()
+        if (straightFlush.isNotEmpty() && straightFlush[0] == 'T' && straightFlush[4] == 'A') {
+            return true
+        }
+        return false
+    }
+
+    fun determineHighestHand(): String {
+        val highestCard = Hand(sortedCards).getHighCard()
+        if (Hand(sortedCards).isRoyalFlush()) {
+            return "Royal Flush"
+        } else if (Hand(sortedCards).getStraightFlush().isNotEmpty()) {
+            return "Straight Flush-${Hand(sortedCards).getStraightFlush()}"
+        } else if (Hand(sortedCards).getFourOfAKind() != ' ') {
+            return "Four of a Kind-${Hand(sortedCards).getFourOfAKind()}"
+        } else if (Hand(sortedCards).getFullHouse()[0].isLetterOrDigit() && Hand(sortedCards).getFullHouse()[1].isLetterOrDigit()) {
+            return "Full House-${Hand(sortedCards).getFullHouse()}"
+        } else if (Hand(sortedCards).getFlush() != ' ') {
+            return "Flush-${Hand(sortedCards).getFlush()}"
+        } else if (Hand(sortedCards).getStraight().isNotEmpty()) {
+            return "Straight-${Hand(sortedCards).getStraight()}"
+        } else if (Hand(sortedCards).getThreeOfAKind() != ' ') {
+            return "Three of a Kind-${Hand(sortedCards).getThreeOfAKind()}"
+        } else if (Hand(sortedCards).getTwoPairs().size == 2) {
+            return "Two Pairs-${Hand(sortedCards).getTwoPairs()}"
+        } else if (Hand(sortedCards).getOnePair() != ' ') {
+            return "One Pair-${Hand(sortedCards).getOnePair()}"
+        }
+        return "High Card-$highestCard"
+    }
+}
+
+fun stringToCard(strings: MutableList<String>): MutableList<Card> {
+    var list = mutableListOf<Card>()
+    for (string in strings) {
+        val card = Card(string.toCharArray()[0], string.toCharArray()[1])
+        list.add(card)
+    }
+    return list
+}
+
 fun main() {
     //println(getHands("src/poker.txt"))
     //println(orderHand(mutableListOf("8C", "KS", "KC", "9H", "4S")))
@@ -37,7 +185,26 @@ fun main() {
     //val card2 = Card('8', 'H')
     //println(card1 > card2) // -1
 
-    println(orderHand(mutableListOf("7S", "TH", "4H", "QS", "TD")))
+    //    val strings = mutableListOf<String>()
+//    for (card in orderedCards) {
+//        val cardString = card.face.plus("").plus(card.suit)
+//        strings.add(cardString)
+//    }
+//    println(strings)
+
+    val cards = stringToCard(mutableListOf("QH", "QC", "JS", "3D", "3C"))
+    val highCard = Hand(cards).getHighCard()
+    val onePair = Hand(cards).getOnePair()
+    val twoPairs = Hand(cards).getTwoPairs()
+    val threeOfAKind = Hand(cards).getThreeOfAKind()
+    val straight = Hand(cards).getStraight()
+    val flush = Hand(cards).getFlush()
+    val fullHouse = Hand(cards).getFullHouse()
+    val fourOfAKind = Hand(cards).getFourOfAKind()
+    val straightFlush = Hand(cards).getStraightFlush()
+    val isRoyalFlush = Hand(cards).isRoyalFlush()
+
+    println(Hand(cards).determineHighestHand())
 }
 
 fun getHands(file: String): MutableList<MutableList<String>> {
@@ -50,124 +217,4 @@ fun getHands(file: String): MutableList<MutableList<String>> {
         hands.add(player2)
     }
     return hands
-}
-
-fun orderHand(cards: MutableList<String>): MutableList<String> {
-    println(cards)
-    val orderedCards = mutableListOf<String>()
-    var other = ""
-    for (card in cards) {
-        if (cards.indexOf(card) == 0) {
-            orderedCards.add(0, card)
-            continue
-        } else if (cards.indexOf(card) == cards.size - 1) {
-            other = cards[0]
-        } else {
-            other = cards[(cards.indexOf(card) - 1)]
-        }
-        println("comparing $card and $other")
-
-        val array = card.toCharArray()
-        val face = array[0]
-        val suit = array[1]
-
-        val otherArray = other.toCharArray()
-        val otherFace = otherArray[0]
-        val otherSuit = otherArray[1]
-
-        val card1 = Card(face, suit)
-        val card2 = Card(otherFace, otherSuit)
-
-        val otherIndex = orderedCards.indexOf(other)
-
-        if (card1 < card2) {
-            println("$card < $other")
-            
-            //orderedCards.add(otherIndex, card)
-            println(orderedCards)
-        } else {
-            println("$card >= $other")
-            orderedCards.add(otherIndex + 1, card)
-            println(orderedCards)
-        }
-
-        /*if (card1.compareTo(card2) == -1) { // card < other
-            println("$card < $other")
-            if (otherIndex == 0) {
-                orderedCards.add(otherIndex, card)
-            } else {
-                var possibleOther = orderedCards[otherIndex - 1]
-                var possibleOtherArray = possibleOther.toCharArray()
-                var possibleFace = possibleOtherArray[0]
-                var possibleSuit = possibleOtherArray[1]
-                var possibleCard = Card(possibleFace, possibleSuit)
-                println(orderedCards.indexOf(possibleOther))
-                while (possibleCard >= card1 && orderedCards.indexOf(possibleOther) != 0) {
-                    println("$possibleOther > $card")
-                    possibleOther = orderedCards[orderedCards.indexOf(possibleOther) - 1]
-                    println("card: $card and possible: $possibleOther")
-                    possibleOtherArray = possibleOther.toCharArray()
-                    possibleFace = possibleOtherArray[0]
-                    possibleSuit = possibleOtherArray[1]
-                    possibleCard = Card(possibleFace, possibleSuit)
-                }
-                orderedCards.add(orderedCards.indexOf(possibleOther), card)
-            }
-        } else { // card >= other
-            println("$card >= $other")
-            if (otherIndex == orderedCards.size - 1) {
-                orderedCards.add(otherIndex + 1, card)
-            } else {
-                var possibleOther = orderedCards[otherIndex + 1]
-                var possibleOtherArray = possibleOther.toCharArray()
-                var possibleFace = possibleOtherArray[0]
-                var possibleSuit = possibleOtherArray[1]
-                var possibleCard = Card(possibleFace, possibleSuit)
-                //println(orderedCards.indexOf(possibleOther) == orderedCards.size - 1)
-                while (possibleCard <= card1 && orderedCards.indexOf(possibleOther) != orderedCards.size - 1) {
-                    possibleOther = orderedCards[orderedCards.indexOf(possibleOther) + 1]
-                    println("card: $card and possible: $possibleOther")
-                    possibleOtherArray = possibleOther.toCharArray()
-                    possibleFace = possibleOtherArray[0]
-                    possibleSuit = possibleOtherArray[1]
-                    possibleCard = Card(possibleFace, possibleSuit)
-                }
-                println("last: $possibleOther")
-                orderedCards.add(orderedCards.indexOf(possibleOther), card)
-            }
-        }
-        println(orderedCards)
-    }*/
-    }
-
-    return orderedCards
-}
-
-fun determineHand(cards: MutableList<String>): String {
-    val orderedHand = orderHand(cards)
-    //println(orderedHand)
-
-    val highestCard = orderedHand.get(orderedHand.size - 1)
-    var hand = "High Card: $highestCard"
-
-    //println(highestCard)
-
-    val faces = mutableListOf<Char>()
-    val suits = mutableListOf<Char>()
-    for (card in orderedHand) {
-        val array = card.toCharArray()
-        faces.add(array[0])
-        suits.add(array[1])
-    }
-//    println(faces)
-//    println(suits)
-
-    for (face in faces) {
-        val count = faces.count { it == face }
-        //println("$count - ${face}")
-        if (count == 2) {
-            hand = "One Pair: $face"
-        }
-    }
-    return hand
 }
