b69a6f11215f8665694fc548d648f3fa1e0eeb03
Lindsey <lindsey.xixi.wang@gmail.com>
2020-05-17 20:53:26 -0400

    Euler58 (almost there!)

--- Euler58/src/Euler58.kt
+++ Euler58/src/Euler58.kt
@@ -1,5 +1,5 @@
 fun main() {
-    println(getNums(7))
+    println(getNums())
 }
 
 fun isPrime(num: Int): Boolean {
@@ -13,19 +13,27 @@ fun isPrime(num: Int): Boolean {
     return true
 }
 
-fun ratio()
-
-fun getNums(side_length: Int): MutableList<Int> {
-    val nums = mutableListOf(1)
+fun getNums(): Int {
+    var nums = mutableListOf(1)
     var side = 3
     var num = 1
-    while (side <= side_length) {
+    while (true) {
+        println(side)
         for (i in 1..4) {
             val difference = side - 1
             num += difference
-            nums.add(num)
+            nums = nums.add(num)
+        }
+        var count = 0
+        for (num in nums) {
+            if (isPrime(num)) {
+                count++
+            }
+        }
+        if (count.toDouble() / nums.size.toDouble() < 0.1) {
+            println("HERE")
+            return side
         }
         side += 2
     }
-    return nums
 }
